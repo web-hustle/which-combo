e57f75fe872d463a59cced12aa46cb4994a2b5bc
LDH <free.dev.ldh@gmail.com>
2026-02-05 20:42:57 +0900

    Feat: 버그 수정 - 게임 후 화면 임시 구현 - 재시작시 기존 데이터 삭제

---
+++ src/pages/FinishScreen/index.tsx
@@ -0,0 +1,29 @@
+import Button from "../../components/Button/Button";
+import type { RoomData } from "../../types";
+
+interface Props {
+  roomData: RoomData;
+  myId: string;
+  restartGame: () => void;
+}
+
+export const FinishScreen = ({ roomData, myId, restartGame }: Props) => {
+  if (!roomData.guest) {
+    return;
+  }
+  const isHost = roomData.host.uid === myId;
+
+  const hostData = roomData.host;
+  const guestData = roomData.guest;
+
+  const winnerName =
+    hostData.score > guestData.score ? hostData.nickname : guestData.nickname;
+
+  return (
+    <div>
+      <h1>{winnerName}</h1>
+      <h2>{hostData.score + " : " + guestData.score}</h2>
+      <div>{isHost && <Button label="asdf" onClick={restartGame} />}</div>
+    </div>
+  );
+};
--- src/pages/PlacingScreen/index.tsx
+++ src/pages/PlacingScreen/index.tsx
@@ -1,70 +1,142 @@
 import { useEffect, useState } from "react";
 import Board from "../../components/Board";
-import type { CellInfo, CellType, RoomData } from "../../types";
+import type { CellInfo, CellType, Player, RoomData } from "../../types";
 import { getCellName } from "../../utils/getFunction";
+import styles from "./PlacingScreen.module.scss";
 
 interface Props {
-    roomData: RoomData;
-    myId: string;
-    onPlace: (index: number) => void;
+  roomData: RoomData;
+  myId: string;
+  onPlace: (index: number) => void;
 }
 
 export const PlacingScreen = ({ roomData, myId, onPlace }: Props) => {
-    const isHost = roomData.host.uid === myId;
-    const myData = isHost ? roomData.host : roomData.guest;
-    const opponentData = isHost ? roomData.guest : roomData.host;
+  const isHost = roomData.host.uid === myId;
+  const myData = isHost ? roomData.host : roomData.guest;
+  const opponentData = isHost ? roomData.guest : roomData.host;
 
-    if (!myData || !opponentData) return <div>Error</div>;
+  if (!myData || !opponentData) return <div>Error</div>;
 
-    const myIndex = myData.currentSequenceIndex;
-    const isFinish = myIndex >= 25;
+  const myIndex = myData.currentSequenceIndex;
+  const isFinish = myIndex >= 25;
 
-    if (isFinish) {
-        return (
-            <div style={{ textAlign: "center", padding: "32px" }}>
-                <h2>배치</h2>
-            </div>
-        );
-    }
-
-    const statusBoard: CellType[] = myData.board.map((num) => ({
-        value: num,
-        status: num === 0 ? "default" : "disabled",
-    }));
-
-    const placeIfPossible = (idx: number) => {
-        if (statusBoard[idx].status === "default" && !myData.isReady) {
-            onPlace(idx);
-        }
-    };
-
-    const [lastOpponentCard, setLastOpponentCard] = useState<CellInfo>({ card: 0, boardIndex: -1 });
-
-    useEffect(() => {
-        // 서로 보드에 카드를 입력함
-        if (!opponentData.isReady && !myData.isReady && opponentData.lastPlacedCard) {
-            setLastOpponentCard(opponentData.lastPlacedCard);
-        }
-    }, [opponentData.isReady, myData.isReady]);
-
+  if (isFinish) {
     return (
-        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-            <h3>이번에 배치할 카드</h3>
-            <h1>{roomData.numberSequence[myIndex]}</h1>
-            <Board board={statusBoard} onCellClick={(idx) => placeIfPossible(idx)} />
-            <div style={{ marginTop: '24px', textAlign: 'center', border: '4px solid #333', width: '300px', height: '80px', borderRadius: '8px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                <div style={{ color: '#666', fontSize: '16px', fontWeight: 'bold' }}>상대({opponentData.nickname})의 마지막 배치</div>
-                {lastOpponentCard && lastOpponentCard.boardIndex !== -1 ? (
-                    <div style={{ color: '#d00', fontSize: '20px', fontWeight: 'bolder' }}>{getCellName(lastOpponentCard.boardIndex)} = {lastOpponentCard.card}</div>
-                ) : (
-                    " "
-                )}
-            </div>
-            <div style={{ marginTop: "8px", fontSize: "16px", color: "#d00", height: '26px' }}>
-                <div style={{ transition: 'opacity 0.3s ease-in-out', opacity: opponentData.isReady || myData.isReady ? 1 : 0 }}>
-                    {opponentData.isReady ? "(상대방 배치 완료 대기 중)" : myData.isReady ? "(배치 완료 대기 중)" : ""}
-                </div>
-            </div>
-        </div>
+      <div style={{ textAlign: "center", padding: "32px" }}>
+        <h2>배치</h2>
+      </div>
     );
+  }
+
+  const statusBoard: CellType[] = myData.board.map((num) => ({
+    value: num,
+    status: num === 0 ? "default" : "disabled",
+  }));
+
+  const placeIfPossible = (idx: number) => {
+    if (statusBoard[idx].status === "default" && !myData.isReady) {
+      onPlace(idx);
+    }
+  };
+
+  const [lastOpponentCard, setLastOpponentCard] = useState<CellInfo>({
+    card: 0,
+    boardIndex: -1,
+  });
+
+  useEffect(() => {
+    // 서로 보드에 카드를 입력함
+    if (
+      !opponentData.isReady &&
+      !myData.isReady &&
+      opponentData.lastPlacedCard
+    ) {
+      setLastOpponentCard(opponentData.lastPlacedCard);
+    }
+  }, [opponentData.isReady, myData.isReady]);
+
+  return (
+    <div className={styles.placing_screen}>
+      <div className={styles.score_area}>
+        <div className={styles.number_area}>
+          <div className={styles.card}>{roomData.numberSequence[myIndex]}</div>
+        </div>
+        <div className={styles.desc_area}>
+          <div
+            style={{ marginBottom: "10px", fontSize: "20px", fontWeight: "600" }}
+          >
+            카드배치 {myData.currentSequenceIndex + 1}턴
+          </div>
+          <LoadingText playerData={myData}></LoadingText>
+          <LoadingText playerData={opponentData}></LoadingText>
+        </div>
+      </div>
+      <Board board={statusBoard} onCellClick={(idx) => placeIfPossible(idx)} />
+      <div
+        style={{
+          marginTop: "24px",
+          textAlign: "center",
+          border: "4px solid #333",
+          width: "300px",
+          height: "80px",
+          borderRadius: "8px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div style={{ color: "#666", fontSize: "16px", fontWeight: "bold" }}>
+          상대({opponentData.nickname})의 마지막 배치
+        </div>
+        {lastOpponentCard && lastOpponentCard.boardIndex !== -1 ? (
+          <div
+            style={{ color: "#d00", fontSize: "20px", fontWeight: "bolder" }}
+          >
+            {getCellName(lastOpponentCard.boardIndex)} = {lastOpponentCard.card}
+          </div>
+        ) : (
+          " "
+        )}
+      </div>
+      <div
+        style={{
+          marginTop: "8px",
+          fontSize: "16px",
+          color: "#d00",
+          height: "26px",
+        }}
+      >
+        <div
+          style={{
+            transition: "opacity 0.3s ease-in-out",
+            opacity: opponentData.isReady || myData.isReady ? 1 : 0,
+          }}
+        >
+          {opponentData.isReady
+            ? "(상대방 배치 완료 대기 중)"
+            : myData.isReady
+            ? "(배치 완료 대기 중)"
+            : ""}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+interface LoadingTextProps {
+  playerData: Player;
+}
+
+const LoadingText = ({ playerData }: LoadingTextProps) => {
+  return (
+    <div style={{ marginTop: "2px" }}>
+      {playerData.nickname}{" "}
+      {playerData.isReady ? (
+        <span style={{ color: "green" }}>배치완료 ✔</span>
+      ) : (
+        <span>고민중..</span>
+      )}
+    </div>
+  );
 };
